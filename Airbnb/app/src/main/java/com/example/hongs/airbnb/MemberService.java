b9fe6b14d025bf93c436f7068b3dc6ffebf3c744
hongs890 <hongs890@gmail.com>
2016-08-04 11:34:04 +0900

    승주형 바보

---
+++ Airbnb/app/src/main/java/com/example/hongs/airbnb/MemberService.java
@@ -0,0 +1,16 @@
+package com.example.hongs.airbnb;
+
+import java.util.List;
+
+public interface MemberService {
+    public String regist(MemberBean mem);
+    public void update(MemberBean mem);
+    public void delete(MemberBean mem);
+    public MemberBean findById(String id);
+    public boolean login(MemberBean member);
+    public void logout(MemberBean member);
+    public int count();
+    public List<MemberBean> list();
+    public MemberBean show();
+    public List<MemberBean> findBy(String keyword);
+}
